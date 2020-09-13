0f4827b5af5b22211fe6b9c7ef2186745bd8ccd0
Migracode <sanchezgalejandroj@gmail.com>
2020-09-14 01:47:24 +0200

    working on it fixing problems

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+const formidable = require('express-formidable');
 const {Pool} = require('pg');
 
 const pool = new Pool({
@@ -12,6 +12,9 @@ const pool = new Pool({
     port: 5432
 })
 
+app.use(formidable())
+app.use(express.static("public"));
+
 app.get('/get-table',(req,res)=>{
     console.log("entro al get");
     pool
@@ -19,6 +22,29 @@ app.get('/get-table',(req,res)=>{
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e));
 })
+
+app.post('/create-buddy',function (req,res) {
+    const name = req.fields.name;
+    const dateofbirth = req.fields.birth;
+    const email = req.fields.email;
+    const hometwon = req.fields.hometwon;
+    const hobbiesandinterests = req.fields.hobbie;
+    const im_a_buddy = req.fields.im_a_buddy;
+    const joined_at = new Date();
+    const query = "INSERT INTO buddies (name,dateofbirth,email,hometown,hobbiesandinterests, im_a_buddy,joined_at) VALUES ($1, $2, $3 ,$4, $5, $6,$7 )"
+    
+    pool
+    .query(query,[name,dateofbirth,email,hometwon,hobbiesandinterests,im_a_buddy,joined_at])
+    .then(()=> res.send("object Create"))
+    .catch((e) => console.log(e))
+    console.log(req.fields.name);
+})
+
+
+app.post('http://localhost:9000/create-patients',function () {
+    
+})
+
 
 app.listen(9000,function () {
     console.log('I am ready to rock');
