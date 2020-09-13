f50d4f3ee6abfe3435a934136e9ddd39065e4c20
Anandamaya <arnoananda@gmail.com>
2020-09-13 20:36:45 +0200

    adding and using the cors so we are able to fetch

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
+
+app.use(cors());
 
 const {Pool} = require('pg');
 
@@ -12,10 +14,14 @@ const pool = new Pool({
     port: 5432
 })
 
+
+
 app.get('/get-table',(req,res)=>{
+    const query = "select * from buddies b union select * from patients p";
+    
     console.log("entro al get");
     pool
-    .query('select * from buddies union select * from patients;')
+    .query(query)
     .then((result) => res.json(result.rows))
     .catch((e) => console.error(e));
 })
