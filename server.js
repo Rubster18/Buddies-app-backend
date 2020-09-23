b405a1016025ff1bf610cb1fae4f32bfefef85d7
Anandamaya <arnoananda@gmail.com>
2020-09-23 18:25:30 +0200

    changing the buddies and patients table to have enabled and disabled and the query for the delete

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(formidable())
 app.use(express.static("public"));
 
 app.get('/get-table',(req,res)=>{
-    const query = "select * from buddies b union select * from patients p order by joined_at desc";
+    const query = "select * from buddies b where enabled = 1 union select * from patients p where enabled = 1 order by joined_at desc";
     
     console.log("entro al get");
     pool
@@ -35,18 +35,41 @@ app.post('/create-user',function (req,res) {
     const hometown = req.fields.hometown;
     const hobbiesandinterests = req.fields.hobbies;
     const im_a_buddy = req.fields.im_a_buddy;
+    const enabled = 1;
+
     const joined_at = new Date();
     let query;
+
     if(im_a_buddy){
-        query = "INSERT INTO buddies (name,dateofbirth,email,hometown,hobbiesandinterests, im_a_buddy,joined_at) VALUES ($1, $2, $3 ,$4, $5, $6,$7 )"
+        query = "INSERT INTO buddies (name,dateofbirth,email,hometown,hobbiesandinterests, im_a_buddy,joined_at, enabled) VALUES ($1, $2, $3 ,$4, $5, $6,$7,$8 )"
     } else{
-        query ="INSERT INTO patients (name,dateofbirth,email,hometown,hobbiesandinterests, im_a_buddy,joined_at) VALUES ($1, $2, $3 ,$4, $5, $6,$7 )"
+        query ="INSERT INTO patients (name,dateofbirth,email,hometown,hobbiesandinterests, im_a_buddy,joined_at, enabled) VALUES ($1, $2, $3 ,$4, $5, $6,$7,$8 )"
     }
     
     pool
-    .query(query,[name,dateofbirth,email,hometown,hobbiesandinterests,im_a_buddy,joined_at])
-    .then(()=> res.send("object Create"))
+    .query(query,[name,dateofbirth,email,hometown,hobbiesandinterests,im_a_buddy,joined_at,enabled])
+    .then(()=> res.json(result.rows))
     .catch((e) => console.log(e))
+})
+
+app.put('/disable-user', (req, res) => {
+    console.log("Disabling an user");
+    // console.log("Receiving: >>>>", req);
+    console.log(req.fields);
+
+    const isBuddy = req.fields.isBuddy;
+    const id = req.fields.id;
+
+    let query = "";
+    
+    isBuddy === 0? query = "UPDATE patients SET enabled=0 WHERE id=$1" : query = "UPDATE buddies SET enabled=0 WHERE id=$1";
+
+    pool
+    .query(query,[id])
+    .then( console.log("disabling user done"))
+    .catch((e) => console.log(e));
+
+    res.send("Done");
 })
 
 
